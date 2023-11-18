7691e071eca487c423bf1fdc683802279df2103d
brenodesouzaa <147631039+brenodesouzaa@users.noreply.github.com>
2023-11-18 17:10:32 -0300

    add: new modal page

--- frontend/src/pages/blog.tsx
+++ frontend/src/pages/blog.tsx
@@ -87,6 +87,64 @@ export default function Blog() {
           </Modal>
         </div>
       </div>
+
+      <div className={styles.buttons_not}>
+        <div
+          style={{
+            background: 'gray',
+            display: 'inline-block',
+            borderRadius: '8px',
+            textAlign: 'left',
+            padding: '10px',
+            margin: '10px 0',
+            maxWidth: '1000px',
+          }}
+        >
+          <div
+            style={{
+              fontSize: '24px',
+              fontWeight: 'bold',
+              marginBottom: '10px',
+            }}
+          >
+            Lorem ipsum dolor
+          </div>
+          <span style={{ fontSize: '14px' }}>
+            {' '}
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+            commodo, purus et tincidunt viverra, libero lectus egestas tortor,
+            laoreet porta magna felis eget dolor. Etiam in metus eu arcu
+            pulvinar auctor ut quis nibh. Praesent auctor et nisl in euismod.
+            Maecenas nec urna pulvinar, maximus leo sit amet, posuere sapien.
+            Quisque at massa libero. Integer dignissim metus quis lectus porta
+            iaculis. Nam turpis lectus, maximus in ornare vitae, bibendum id
+            nulla.{' '}
+          </span>
+
+          <Button colorScheme="teal" mt={4} onClick={handleOpen}>
+            Abrir
+          </Button>
+
+          <Modal isOpen={isOpen} onClose={handleClose} size="xl">
+            <ModalOverlay />
+            <ModalContent style={{ background: '#4e5252', color: 'white', width: '90%', maxWidth: '1200px' }}>
+              <ModalHeader>Lorem ipsum dolor</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                commodo, purus et tincidunt viverra, libero lectus egestas
+                tortor, laoreet porta magna felis eget dolor. Etiam in metus eu
+                arcu pulvinar auctor ut quis nibh. Praesent auctor et nisl in
+                euismod. Maecenas nec urna pulvinar, maximus leo sit amet,
+                posuere sapien. Quisque at massa libero. Integer dignissim
+                metus quis lectus porta iaculis. Nam turpis lectus, maximus in
+                ornare vitae, bibendum id nulla.
+              </ModalBody>
+            </ModalContent>
+          </Modal>
+        </div>
+      </div>
+
     </>
   );
 }
