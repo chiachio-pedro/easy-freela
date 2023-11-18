4d978e56d9f41ef3ce1970fef40054880879b8c5
brenodesouzaa <147631039+brenodesouzaa@users.noreply.github.com>
2023-11-18 16:25:37 -0300

    botafogo de futebol e regatas

--- frontend/src/pages/blog.tsx
+++ frontend/src/pages/blog.tsx
@@ -3,27 +3,31 @@ import { Button } from '@chakra-ui/react'
 
 export default function Blog(){
     return(
-        <><div className={styles.blog_container1}>
-
-            <div className={styles.blog_home}>
-                <h1 className={styles.title}>Blog</h1>
-                <br></br>
-                <p>Praesent cursus bibendum nisl tempor efficitur. Pellentesque eget aliquet lorem. Praesent fringilla justo sit amet diam sollicitudin elementum. Mauris sed pharetra.</p>
-                <br></br>
+        <>
+            <div className={styles.blog_container1}>
+                <div className={styles.blog_home}>
+                    <h1 className={styles.title}>Blog</h1>
+                    <br></br>
+                    <p>Praesent cursus bibendum nisl tempor efficitur. Pellentesque eget aliquet lorem. Praesent fringilla justo sit amet diam sollicitudin elementum. Mauris sed pharetra.</p>
+                    <br></br>
+                </div>
             </div>
-        </div>
-        
-        <div className={styles.buttons_not}> 
-    <div style={{ background: 'gray', display: 'inline-block', borderRadius: '8px', textAlign: 'left', padding: '10px', margin: '10px 0' }}>
-        <div style={{ fontSize: '24px', fontWeight: 'bold', marginBottom: '10px' }}>Lorem ipsum dolor</div>
-        <br></br>
-        Noticia/Imagem - MODALNoticia/Imagem - MODALNoticia/Imagem - MODALNoticia/Imagem - MODAL
-    </div>
-</div>
 
-</>
+            <div className={styles.buttons_not}> 
+                <div style={{ background: 'gray', display: 'inline-block', borderRadius: '8px', textAlign: 'left', padding: '10px', margin: '10px 0' }}>
+                    <div style={{ fontSize: '24px', fontWeight: 'bold', marginBottom: '10px' }}>Lorem ipsum dolor</div>
+                    <br></br>
+                    Noticia/Imagem - MODALNoticia/Imagem - MODALNoticia/Imagem - MODALNoticia/Imagem - MODAL
+                </div>
+            </div>
 
-        
-        
+            <div className={styles.buttons_not}> 
+                <div style={{ background: 'gray', display: 'inline-block', borderRadius: '8px', textAlign: 'left', padding: '10px', margin: '10px 0' }}>
+                    <div style={{ fontSize: '24px', fontWeight: 'bold', marginBottom: '10px' }}>Lorem ipsum dolor</div>
+                    <br></br>
+                    Noticia/Imagem - MODALNoticia/Imagem - MODALNoticia/Imagem - MODALNoticia/Imagem - MODAL
+                </div>
+            </div>
+        </>
     )
 }
